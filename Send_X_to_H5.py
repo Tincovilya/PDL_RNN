cc80f1ec6cafe6e62e7c0ce7dfb72910fedbd6b0
Nic <nic.pat.smith@outlook.com>
2018-09-07 11:49:45 -0600

    Store X in h5
    Actually made the module do what I needed. This will now store the Xs in an h5 for easier retrieval later.

--- Send_X_to_H5.py
+++ Send_X_to_H5.py
@@ -2,10 +2,30 @@
 """
 Created on Tue Sep  4 15:18:14 2018
 
-@author: Administrator
+@author: Nic Smith
 
-This module literall just takes the X's from Build_Examples and turns them
+This module literally just takes the X's from Build_Examples and turns them
 into an h5 array, because that makes everything way more effecient.
 """
 
 import pandas as pd
+import numpy as np
+import Build_Examples
+from tqdm import tqdm
+
+def Send_X_To_h5():
+    #First get the times from the AGM list
+    times = Build_Examples.insert_examples()
+
+    #Next build all of the examples (this takes about 20 minutes)
+    X=[]
+    for j in tqdm(times):
+        X.append(Build_Examples.get_xs(j[0],j[1]))
+
+    #Write information into a dataframe
+    df = pd.DataFrame.from_records(X)
+
+    print(df)
+
+    store = pd.HDFStore("X_Examples.h5")
+    store.append("X", df)
